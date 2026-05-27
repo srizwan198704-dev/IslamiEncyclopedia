17728e8e3951dae07010add2cdaf17aecdaad109
SR Rizwan <srizwan198704@gmail.com>
2026-05-27 12:23:59 +0600

    Update ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/srizwan/islamipedia/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/srizwan/islamipedia/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.srizwan.bookhozur14", appContext.packageName)
+        assertEquals("com.srizwan.islamipedia", appContext.packageName)
     }
 }
